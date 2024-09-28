fd402cadd3e85a11e823690c627f034bfb835df0
Geovanny Mendoza <mgeovanny@uninorte.edu.co>
2024-09-28 17:08:31 -0500

    Add first activity in android

---
+++ app/src/main/java/com/geovannycode/androidmaster/firstapp/FirstAppActivity.kt
@@ -0,0 +1,38 @@
+package com.geovannycode.androidmaster.firstapp
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.geovannycode.androidmaster.R
+
+class FirstAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_fisrt_app)
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etName = findViewById<AppCompatEditText>(R.id.etName)
+
+        btnStart.setOnClickListener {
+            val name = etName.text.toString()
+
+            if (name.isNotEmpty()) {
+                val intent = Intent(this, ResultActivity::class.java)
+                intent.putExtra("EXTRA_NAME", name)
+                startActivity(intent)
+            }
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/geovannycode/androidmaster/firstapp/ResultActivity.kt
+++ app/src/main/java/com/geovannycode/androidmaster/firstapp/ResultActivity.kt
@@ -1,17 +1,23 @@
 package com.geovannycode.androidmaster.firstapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.geovannycode.androidmaster.R
 
-class FisrtAppActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_fisrt_app)
+        setContentView(R.layout.activity_result)
+
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val name: String = intent.extras?.getString("EXTRA_NAME").orEmpty()
+        tvResult.text = "Hola $name"
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
