1287a57f7ea0338c82917594de19a279120dc412
Geovanny Mendoza <mgeovanny@uninorte.edu.co>
2024-10-02 23:52:59 -0500

    Add New Example Calculator

--- app/src/main/java/com/geovannycode/androidmaster/MenuActivity.kt
+++ app/src/main/java/com/geovannycode/androidmaster/MenuActivity.kt
@@ -4,21 +4,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
-import com.geovannycode.androidmaster.firstapp.FirstAppActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         val btnSaludApp = findViewById<Button>(R.id.btnSaludApp)
-        //val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
+        val btnImcApp = findViewById<Button>(R.id.btnIMCApp)
         //val btnTODO = findViewById<Button>(R.id.btnTODO)
         //val btnSuperhero = findViewById<Button>(R.id.btnSuperhero)
         //val btnSettings = findViewById<Button>(R.id.btnSettings)
 
-        btnSaludApp.setOnClickListener { navigateToSaludApp() }
-        //btnImcApp.setOnClickListener { navigateToImcApp() }
+       // btnSaludApp.setOnClickListener { navigateToSaludApp() }
+        btnImcApp.setOnClickListener { navigateToImcApp() }
         //btnTODO.setOnClickListener { navigateToTodoApp() }
         //btnSuperhero.setOnClickListener { navigateToSuperheroApp() }
         //btnSettings.setOnClickListener { navigateToSettings() }
@@ -33,17 +31,18 @@ class MenuActivity : AppCompatActivity() {
         val intent = Intent(this, TodoActivity::class.java)
         startActivity(intent)
     }
-
+*/
     private fun navigateToImcApp() {
-        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        val intent = Intent(this, ImcCalculatorActivity1::class.java)
         startActivity(intent)
     }
-*/
+
+/*
     private fun navigateToSaludApp(){
         val intent = Intent(this, FirstAppActivity::class.java)
         startActivity(intent)
     }
-/*
+
     private fun navigateToSuperheroApp(){
         val intent = Intent(this, SuperHeroListActivity::class.java)
         startActivity(intent)
